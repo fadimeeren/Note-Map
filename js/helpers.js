3a68c2f0cfd03d0f296f4d4677845a66119f4440
fadimeeren <fadime_ern135@outlook.com>
2025-08-27 11:22:05 +0300

    Güncellendi

---
+++ js/helpers.js
@@ -0,0 +1,55 @@
+
+import { defaultIcon, visitIcon, homeIcon, jobIcon, parkIcon } from "./ui.js";
+
+//* Note'un status değerine göre icon belirler
+export function getNoteIcon(status) {
+  switch (status) {
+    case "visit":
+      return visitIcon;
+    case "home":
+      return homeIcon;
+    case "job":
+      return jobIcon;
+    case "park":
+      return parkIcon;
+    default:
+      return defaultIcon;
+  }
+}
+
+//* Tarih verisini ƒormatlayan fonksiyon
+export const formatDate = (date) => {
+  return new Date(date).toLocaleDateString("tr", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+};
+
+//* Status değerine göre ekrana basılacak metni belirle
+export const getStatus = (status) => {
+  switch (status) {
+    case "visit":
+      return "Ziyaret";
+
+    case "park":
+      return "Park yeri";
+
+    case "home":
+      return "Ev";
+
+    case "job":
+      return "İş";
+
+    default:
+      "Tanımsız";
+  }
+};
+
+// aynı sorun farklı çözüm
+export const statusObj = {
+  visit: "Ziyaret",
+  park: "Park yeri",
+  home: "Ev",
+  job: "İş",
+};
--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-import { ui, personIcon } from "./ui.js";
 
-// Global Değişkenler
+import { ui, personIcon } from "./ui.js";
+import { getNoteIcon, formatDate, getStatus, statusObj } from "./helpers.js";
+
+//* Global Değişkenler
 const STATE = {
   map: null,
   layer: null,
   clickedCoords: null,
-  notes:[],
+  notes: JSON.parse(localStorage.getItem("notes") || "[]"),
 };
 
-// Kullanıcının konumuna göre haritayı yükle
+//* Kullanıcının konumuna göre haritayı yükle
 window.navigator.geolocation.getCurrentPosition(
-  // Kullanıcı iizn verirse onun olduğu konuma
+  // kullanıcı izin verirse onun olduğu konumda yükle
   (e) => loadMap([e.coords.latitude, e.coords.longitude]),
-  // izin vermezse varsayılan olarak İstanbul odaklı yükle
+  // izin vermezse varsayılan olarak istanbul odaklı yükle
   () => loadMap([41.104187, 29.051014])
 );
 
-//  Leaflet haritasının kurulumunu yap
+//* Leaflet haritasının kulumunu yapar
 function loadMap(position) {
+  // haritanın kurulumu
   STATE.map = L.map("map", { zoomControl: false }).setView(position, 11);
 
   // haritaya arayüz ekle
@@ -25,73 +28,173 @@ function loadMap(position) {
     maxZoom: 19,
   }).addTo(STATE.map);
 
-  // Kontrolcüyü sağ alta taşı
+  // kontrolcüyü sağ alta taşı
   L.control.zoom({ position: "bottomright" }).addTo(STATE.map);
 
-  // Harita üzerine bir layer oluştur
+  // harita üzerinde bir layer oluştur
   STATE.layer = L.layerGroup().addTo(STATE.map);
 
-  // Ekrana marker bas
+  // ekrana marker bas
   const marker = L.marker(position, { icon: personIcon }).addTo(STATE.map);
 
-  // Marker'a popup ekle
-  marker.bindPopup("<b>Buradasın<b/><br/>");
+  // marker'a popup ekle
+  marker.bindPopup("<b>Buradasın</b>");
 
-  // Haritaya tıklanma olayı  için izleyici ekle
+  // haritaya tıklama olayı için izlyici ekle
   STATE.map.on("click", onMapClick);
-};
 
-// Haritaya tıklanınca çalışır
+  // notları ekrana bas
+  renderNoteCards(STATE.notes);
+  renderMarker(STATE.notes);
+}
+
+//* Haritaya tıklanınca çalışır
 function onMapClick(e) {
-  STATE.coords = [e.latlng.lat, e.latlng.lng];
+  // son tıklanılan konumu kaydet
+  STATE.clickedCoords = [e.latlng.lat, e.latlng.lng];
 
-  // Aside alanındaki formu aktif hale getir
+  // aside alanındaki formu aktif hale getir
   ui.aside.classList.add("add");
 
-  // Aside alanındaki başlığı güncelle
+  // aside alanındaki başlığı güncelle
   ui.asideTitle.textContent = "Yeni Not";
+}
+
+//* İptal butonuna tıklanınca
+ui.cancelButton.addEventListener("click", () => {
+  // aside alanını ekleme modundan çıkar
+  ui.aside.classList.remove("add");
+
+  // title'ı eski haline getir
+  ui.asideTitle.textContent = "Notlar";
+});
+
+//* Ok'a tıklanınca aside alanını aç/kapa
+ui.arrow.addEventListener("click", () => {
+  ui.aside.classList.toggle("hide");
+});
+
+//* Form gönderilince
+ui.form.addEventListener("submit", (e) => {
+  // sayfa yenilemeyi engelle
+  e.preventDefault();
+
+  // formdaki verilere eriş
+  const title = e.target[0].value;
+  const date = e.target[1].value;
+  const status = e.target[2].value;
+
+  // eğer form doldurulmadıysa kullanıcıya uyarı ver
+  if (!title || !date || !status) {
+    return alert("Lütfen formu doldurunuz!");
+  }
+
+  // kaydedilecek nesneyi oluştur
+  const newNote = {
+    id: new Date().getTime(),
+    title,
+    date,
+    status,
+    coords: STATE.clickedCoords,
+  };
+
+  // notes dizisine yeni notu ekl
+  STATE.notes.push(newNote);
+
+  // localStorage'ı güncelle
+  localStorage.setItem("notes", JSON.stringify(STATE.notes));
+
+  // ekleme modunu kapat
+  ui.aside.classList.remove("add");
+  ui.asideTitle.textContent = "Notlar";
+
+  // notları ekrana bas
+  renderNoteCards(STATE.notes);
+  renderMarker(STATE.notes);
+});
+
+//* note marker'larını ekrana bas
+function renderMarker(notes) {
+  // haritadaki katmana daha önceden eklenmiş marker'ları temizle
+  STATE.layer.clearLayers();
+
+  // notes dizisindeki her bir not için ekrana bir marker bas
+  notes.forEach((note) => {
+    // note'un icon'ını belirle
+    const icon = getNoteIcon(note.status);
+
+    // marker oluştur
+    const marker = L.marker(note.coords, { icon }).addTo(STATE.layer);
+
+    // note'ların başlığını popup olarak marker'a ekle
+    marker.bindPopup(`<p class="popup">${note.title}<p>`);
+  });
+}
+
+//* note card'larını ekrana bas
+function renderNoteCards(notes) {
+  // notes dizisindeki her nesneyi dönerek bir li elemanı oluştur
+  const notesHtml = notes
+    .map(
+      (note) => `
+        <li>
+          <div>
+            <h3>${note.title}</h3>
+            <p>${formatDate(note.date)}</p>
+            <p class="status">${getStatus(note.status)}</p>
+          </div>
+          <div class="icons">
+            <i data-id="${note.id}" id="fly-btn" class="bi bi-airplane-fill"></i>
+            <i data-id="${note.id}" id="trash-btn" class="bi bi-trash"></i>
+          </div>
+        </li> `
+    )
+    .join(" ");
+
+  // oluşturulan note elemanlarını ekrana bas
+  ui.noteList.innerHTML = notesHtml;
+
+  // delete btn'lara eriş
+  document.querySelectorAll("#trash-btn").forEach((btn) => {
+    // butonun bağlı olduğu note'un id'sine eriş
+    const id = +btn.dataset.id;
+
+    // butona tıklanma olayını izle
+    btn.addEventListener("click", () => deleteNote(id));
+  });
+
+  // fly btn'lara eriş
+  document.querySelectorAll("#fly-btn").forEach((btn) => {
+    // butonun bağlı olduğu note'un id'sine eriş
+    const id = +btn.dataset.id;
+
+    // butona tıklanma olayını izle
+    btn.addEventListener("click", () => flyToNote(id));
+  });
+}
+
+// notu silen fonksiyon
+const deleteNote = (id) => {
+  // kullanıcadan onay vermzse dur
+  if (!confirm("Notu silmek istediğinizden emin misiniz?")) return;
+
+  // id'si bilinen note'u diziden kaldır
+  STATE.notes = STATE.notes.filter((note) => note.id !== id);
+
+  // localstrage'ı güncelle
+  localStorage.setItem("notes", JSON.stringify(STATE.notes));
+
+  // arayüzü güncelle
+  renderMarker(STATE.notes);
+  renderNoteCards(STATE.notes);
 };
 
-// İptal butonuna basınca 
-ui.cancelButton.addEventListener("click", () => {
-    // aside alanını ekleme modundan çıkar
-    ui.aside.classList.remove("add");
+// note'u haritada gösteren fonksiyon
+const flyToNote = (id) => {
+  // tıklanılan note'un verilerine eriş
+  const note = STATE.notes.find((note) => note.id === id);
 
-    // Title'ı eski haline getir
-    ui.asideTitle.textContent = "Notlar";
-});
+  // haritada göster
+  STATE.map.flyTo(note.coords, 15);
+};
 
-// Ok'a tıklanınca aside alanını aç/kapa
-ui.arrow.addEventListener("click", ()=> {
-    ui.aside.classList.toggle("hide");
-});
-
-
-// Form gönderilince
-ui.form.addEventListener("submit", (e) => {
-    // Sayfa yenilemeyi engelle
-    e.preventDefault();
-
-    // Formdaki verilere eriş
-    const title = e.target[0].value;
-    const date = e.target[1].value;
-     const statu = e.target[2].value;
-
-
-     // Eğer form doldurulmadıysa kullanıcıya uyarı ver
-     if(!title || !date || !status) {
-       return alert ("Lütfen formu doldurunuz!");
-     };
-
-     // Kaydedilecek nesneyi oluştur
-     const newNote = {
-        id: new Date().getTime(),
-        title,
-        date,
-        status,
-        coords: STATE.clickedCoords,
-     };
-
-
-      console.log(newNote);
-});
